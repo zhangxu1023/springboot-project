a9f02f9eae356b305ce47885f135ecd7fb6ed63b
zhangx <zhangx@51youse.com>
2021-06-02 17:19:51 +0800

    up

--- jht/api-server/src/main/java/com/jht/apiserver/ApiServerApplication.java
+++ jht/api-server/src/main/java/com/jht/apiserver/ApiServerApplication.java
@@ -3,14 +3,21 @@ package com.jht.apiserver;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages = "com.jht")
 @MapperScan(value = "com.jht.*.*.dao")
-public class ApiServerApplication {
+public class ApiServerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiServerApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ApiServerApplication.class);
     }
 
 }
